f59cb8683d40422cac37ada518a053725edec2d3
yoobin <rladbdms0581@naver.com>
2017-12-04 13:39:57 +0900

    pa8_60171627_김유빈 프로젝트 첫 commit

--- Drawable.java
+++
@@ -1,8 +0,0 @@
-
-public interface Drawable {
-	void draw();
-
-}
-
-//60171627 ±Ë¿Ø∫Û
-//interface∏¶ ¿€º∫«—¥Ÿ.
--- Movable.java
+++
@@ -1,7 +0,0 @@
-
-public interface Movable {
-	void move(double dx, double dy);
-}
-
-//60171627 ±èÀ¯ºó
-//Movable interface¸¦ »ý¼ºÇÑ´Ù.
